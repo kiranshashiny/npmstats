97ffc5e21c9d1dc8845b57fe0ec58f4147fcaeef
shkiran4 <shkiran4@in.ibm.com>
2017-05-31 11:38:19 +0530

    Initial Commit

---
+++ getstats.js
@@ -0,0 +1,18 @@
+
+//registry = require('npm-stats')(["https://npm-registry.whitewater.ibm.com/", ""])
+
+//#registry = require('npm-stats')(["https://registry.npmjs.com/", ""])
+
+//registry.user("coe").list().on('data', (chunk) => {  console.log(chunk); });
+
+//registry.list().on('data', (chunk) => {  console.log(chunk); });
+
+// list by date
+//registry.listByDate().on('data', (chunk) => {  console.log(chunk); });
+
+
+// count of modules by user
+//registry.user("bcoe").count().on('data', (chunk) => {  console.log(chunk); });
+
+//  list modules by this user.
+//registry.user("bcoe").list().on('data', (chunk) => {  console.log(chunk); });
