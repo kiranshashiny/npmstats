bbb131a07d9f4e4371fb1b988e4df97187fdd832
shkiran4 <shkiran4@in.ibm.com>
2017-06-05 11:59:19 +0530

    Updated with code to read from the couch Database

--- getstats.js
+++ getstats.js
@@ -1,18 +1,27 @@
 
-//registry = require('npm-stats')(["https://npm-registry.whitewater.ibm.com/", ""])
+registry = require('npm-stats')(["https://npm-registry.whitewater.ibm.com/", "downloads"])
+// this lists everything package that is in the registry .
+// registry.list().on('data', (chunk) => {  console.log(chunk); });
 
 //#registry = require('npm-stats')(["https://registry.npmjs.com/", ""])
 
-//registry.user("coe").list().on('data', (chunk) => {  console.log(chunk); });
+//registry.user("bcoe").list().on('data', (chunk) => {  console.log(chunk); });
 
-//registry.list().on('data', (chunk) => {  console.log(chunk); });
 
-// list by date
-//registry.listByDate().on('data', (chunk) => {  console.log(chunk); });
-
+// list by date - I cant get the since date working - maybe it is the format !
+//registry.listByDate( "since 01-01-2017").on('data', (chunk) => {  console.log(chunk); });
 
 // count of modules by user
-//registry.user("bcoe").count().on('data', (chunk) => {  console.log(chunk); });
+registry.user("bcoe").count().on('data', (chunk) => {  console.log(chunk); });
 
 //  list modules by this user.
-//registry.user("bcoe").list().on('data', (chunk) => {  console.log(chunk); });
+//registry.user("shkiran4").list().on('data', (chunk) => {  console.log(chunk); });
+
+console.log ("list all modules by keyword npm");
+registry.keyword('npm').list().on('data', (chunk) => {  console.log(chunk); });
+
+// Get the version of the module
+
+//console.log ("getting the version");
+//registry.module('somedir').version("").on('data', ( chunk) => {  console.log(chunk); });
+
