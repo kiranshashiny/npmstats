026c26e262e25fa562e539996ffdf84f3e2a6a09
shkiran4 <shkiran4@in.ibm.com>
2017-06-05 12:53:52 +0530

    code updated with comments

--- couch.js
+++ couch.js
@@ -1,8 +1,16 @@
-// http://www.dbarnes.info/node-couchdb-api/#server
-// https://www.npmjs.com/package/couchdb-api
-//
-//
-//
+#APIs here were used from http://www.dbarnes.info/node-couchdb-api/#server
+#
+# This code can be run where the NPME is installed.
+# This queries the couch db and lists the contents of the database.
+# To run this ; node couch.js
+# Set the IP address of the server correctly, apparently the IP of localhost 127.0.0.1 did not work.
+# so I had to set the other IP address from 'ifconfig |grep inet' to connect to the couch db.
+#
+# Code has to be changed to the appropriate database names, but in my case I am reading from 
+# _users, _registry databases.
+#
+#
+
 var couchdb = require("couchdb-api");
 
 // connect to a couchdb server (defaults to http://localhost:5984)
@@ -20,11 +28,11 @@ server.info(function (err, response) {
 
 });
 // this is good.
-console.log ("connected");
-console.log ("Get the databases in it ");
+console.log ("connection to the database was successful");
+console.log ("Get the databases in it. ");
 
 server.allDbs( function (err, response) {
-    console.log ("*************** Listing all the databses, should be _replicator, _users. and registry");
+    console.log ("*************** Listing all the databases, should be _replicator, _users, and registry");
     console.log(response);
     console.log(err);
     console.log ("*************** Done **********");
@@ -36,7 +44,7 @@ server.allDbs( function (err, response) {
 } );
 //log
 server.log( function (err, response) {
-    console.log ("*************** aGet the text of the log *************");
+    console.log ("*************** Get the text of the log *************");
     console.log(response);
     console.log(err);
     console.log ("*************** Done **********");
@@ -51,7 +59,7 @@ server.stats( function (err, response) {
 });
 
 //uuids
-server.stats( function (err, response) {
+server.uuids( function (err, response) {
     console.log ("*************** Server.uuids - Lists of UUIDs ************ ");
     console.log(response);
     console.log(err);
@@ -59,34 +67,23 @@ server.stats( function (err, response) {
 });
 
 
-
-
+/********************************************************************************/
+/*******************************  DATABASE , server->db(_users) *****************/
+/********************************************************************************/
 // select a database, i.e either _replicator, _users. or registry.
 var db = server.db("_users");
 
 db.info(function (err, response) {
-    console.log ("*************** Basic stats of  _users database **************");
-    console.log(response);
-    console.log(err);
-    console.log ("*************** DONE *********************");
+	console.log ("*************** Basic stats of  _users database **************");
+	console.log(response);
+	console.log(err);
+	console.log ("*************** DONE *********************");
 
          // should see the basic statistics for your test database
          // if you chose a non-existant db, you'd get { error: "not_found", reason: "no_db_file" } in place of `err`
 });
 
-// select the registry database now.
-var db = server.db("registry");
-
-db.info(function (err, response) {
-    console.log ("*************** Basic stats of  registry database **************");
-    console.log(response);
-    console.log(err);
-    console.log ("*************** DONE *********************");
-
-});
-
-
-console.log ("print all docs");
+//allDocs
 var docs = db.allDocs(function (err, response) {
     console.log ("*************** printing all the db.allDocs() ********");
     console.log(response);
@@ -95,25 +92,52 @@ var docs = db.allDocs(function (err, response) {
 
 });
 
-/*
-# node couch.js 
-connected
-connected to a db
-{ couchdb: 'Welcome',
-  uuid: '19fb64f5ead46e2f67355775ba34fcbe',
-  version: '1.6.1',
-  vendor: { version: '1.6.1', name: 'The Apache Software Foundation' } }
-null
-{ db_name: '_users',
-  doc_count: 2,
-  doc_del_count: 0,
-  update_seq: 3,
-  purge_seq: 0,
-  compact_running: false,
-  disk_size: 20578,
-  data_size: 8340,
-  instance_start_time: '1492082093673416',
-  disk_format_version: 6,
-  committed_update_seq: 3 }
-*/
+/********************************************************************************/
+/***********Server-> Database-> Document  - look at db.allDocs()   **************/
+/********************************************************************************/
+
+var doc = db.doc("users");
+doc.get(function (err, response) {
+
+    console.log ("*************** info about document.get'users' ********");
+    console.log(response);
+    console.log(err);
+    console.log ("*************** Done   *********************");
+
+});
+/********************************************************************************/
+/*******************************  DATABASE , server->db(registry) ***************/
+/********************************************************************************/
+
+// select the registry database now.
+var db_registry = server.db("registry");
+
+db_registry.info(function (err, response) {
+	console.log ("*************** Basic stats of server.db('registry') database **************");
+	console.log(response);
+	console.log(err);
+	console.log ("*************** DONE *********************");
+
+});
+/********************************************************************************/
+/*********************  DATABASE , server->db(registry)->docs() *****************/
+/********************************************************************************/
+
+//allDocs of db(registry)
+var docs_registry = db_registry.allDocs(function (err, response) {
+    console.log ("*************** printing all the db_registry.allDocs() ********");
+    console.log(response);
+    console.log(err);
+    console.log ("*************** Done   *********************");
+
+});
+
+var doc_registry =  docs_registry.doc("aaa")
+doc_registry.show ('ddoc',  function (err, response) {
+    console.log ("*************** doc_registry - show of aaa ********");
+    console.log(response);
+    console.log(err);
+    console.log ("*************** Done   *********************");
+
+});
 
